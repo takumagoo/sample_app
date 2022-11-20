c19b3388895e89d9ef551dfb84d5be052bfd707c
takumagoo <117556435+takumagoo@users.noreply.github.com>
2022-11-20 06:30:32 +0000

    [start] 3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,13 +5,19 @@ class ListsController < ApplicationController
   end
 
   def create
-     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 4. トップ画面へリダイレクト
-    # redirect_to '/top'
-    redirect_to list_path(list.id)
+    # # １.&2. データを受け取り新規登録するためのインスタンス作成
+    # list = List.new(list_params)
+    # # 3. データをデータベースに保存するためのsaveメソッド実行
+    # list.save
+    # # 4. トップ画面へリダイレクト
+    # # redirect_to '/top'
+    # redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(list.id)
+    else
+      render :new
+    end
   end
 
   def index
