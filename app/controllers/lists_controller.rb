c14468d1ce633dbb12979ec8e25d7164a97b55a5
takumagoo <117556435+takumagoo@users.noreply.github.com>
2022-11-27 07:36:37 +0000

    [start] 3-フラッシュメッセージ

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,13 +5,16 @@ class ListsController < ApplicationController
   end
 
   def create
-    # # １.&2. データを受け取り新規登録するためのインスタンス作成
-    # list = List.new(list_params)
-    # # 3. データをデータベースに保存するためのsaveメソッド実行
-    # list.save
-    # # 4. トップ画面へリダイレクト
-    # # redirect_to '/top'
-    # redirect_to list_path(list.id)
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    # redirect_to '/top'
+    flash[notice] = "投稿が成功しました"
+    
+    redirect_to list_path(list.id)
+  end
     @list = List.new(list_params)
     if @list.save
       redirect_to list_path(@list.id)
@@ -20,7 +23,7 @@ class ListsController < ApplicationController
       # @lists = List.all
       # render :index
     end
-  end
+    
 
   def index
     @lists=List.all
